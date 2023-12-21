9ec21d9fdc6ab9c4bd5628d065c3a42ad97826ff
Camino Losada <camino.losada.alonso@gmail.com>
2023-12-21 16:06:24 +0100

    Create GlobalStyle and add in main

---
+++ src/styles/GlobalStyle/GlobalStyle.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*,::before,::after{
+    box-sizing: border-box;
+  }
+
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0;
+    padding: 0;
+    font: inherit;
+  }
+
+  a{
+    text-decoration: none;
+    color: inherit;
+    font: inherit;
+  }
+
+  ul{
+    list-style: none;
+    padding-left: 0;
+    margin-bottom: 0;
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+    background-color: transparent;
+  }
+
+`;
+
+export default GlobalStyle;
