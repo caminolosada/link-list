b168c84a3f790bddef736c2624fb7459d846d83e
Camino Losada <camino.losada.alonso@gmail.com>
2023-12-29 14:41:48 +0100

    Make Card component responsive

--- src/components/Card/CardStyled.ts
+++ src/components/Card/CardStyled.ts
@@ -11,6 +11,20 @@ const CardStyled = styled.article`
   padding: 40px;
   margin-top: 40px;
   margin-bottom: 90px;
+  min-width: 320px;
+
+  @media (min-width: 400px) {
+    margin: 40px;
+  }
+  @media (min-width: 1080px) {
+    padding: 40px;
+    flex-direction: row;
+    gap: calc(17% - 40px);
+    min-height: 580px;
+    width: calc(60% - 40px);
+    height: auto;
+    max-width: 860px;
+  }
 `;
 
 export default CardStyled;
